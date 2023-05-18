128fec493af0bff11804b244dffe52dc9d9ec06f
wangqineng <wangqineng@baidu.com>
2023-05-18 11:17:55 +0800

    nothing

--- Demo/Rotor37_2d/surrogate_compare/GPR_new.py
+++ Demo/Rotor37_2d/surrogate_compare/GPR_new.py
@@ -2,6 +2,7 @@ import torch
 import os
 import numpy as np
 import gpytorch
+from post_process.load_model import get_noise
 
 class GPRegressionModel(gpytorch.models.ExactGP):
     def __init__(self, train_x, train_y, likelihood):
@@ -14,13 +15,15 @@ class GPRegressionModel(gpytorch.models.ExactGP):
         covar_x = self.covar_module(x)
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
-def get_pred(npz_path, n_train, parameter):
+def get_pred(npz_path, n_train, n_noise, parameter):
     data = np.load(npz_path)
     design = torch.tensor(data["Design"], dtype=torch.double)
     value = torch.tensor(data[parameter].squeeze(), dtype=torch.double)
 
     train_x = design[:n_train]
     train_y = value[:n_train]
+    noise = get_noise(train_y.numpy().shape, n_noise) * np.mean(train_y.numpy())
+    train_y = train_y + torch.tensor(noise, dtype=torch.double)
 
     valid_x = design[-400:]
     valid_y = value[-400:]
@@ -42,10 +45,10 @@ def get_pred(npz_path, n_train, parameter):
         # Output from model
         output = model(train_x)
         # Calc loss and backprop gradients
-        loss = -mll(output, train_y)
+        loss = -1 * mll(output, train_y)
         # loss = sum(loss)
         loss.backward()
-        if i % 5 == 4:
+        if i % 10 == 9:
             print('Iter %d/%d - Loss: %.3f   lengthscale: %.3f   noise: %.3f' % (
                 i + 1, training_iter, loss.item(),
                 model.covar_module.base_kernel.lengthscale.item(),
@@ -69,26 +72,38 @@ def get_pred(npz_path, n_train, parameter):
 
 if __name__ == "__main__":
     npz_path = os.path.join("..", "data", "surrogate_data", "scalar_value.npz")
-    dict_all = {}
+    dict_num = {}
+    dict_noise = {}
 
     parameterList = [
-    "PressureRatioV", "TemperatureRatioV",
-    "Efficiency", "EfficiencyPoly",
-    "PressureLossR", "EntropyStatic",
-    "MachIsentropic", "Load",
-    "MassFlow"]
-
-    n_trainList = [500, 1000, 1500, 2000, 2500]
-
-
+        "PressureRatioV", "TemperatureRatioV",
+        "Efficiency", "EfficiencyPoly",
+        "PressureLossR", "EntropyStatic",
+        "MachIsentropic", "Load",
+        "MassFlow"]
     for parameter in parameterList:
-        data_box = np.zeros([400, 5])
-        for ii,  n_train in enumerate(n_trainList):
-            pred = get_pred(npz_path, n_train, parameter)
-            data_box[:, ii] = pred.copy()
-        dict_all.update({parameter: data_box})
 
-    np.savez(os.path.join("data", "GPR.npz"), **dict_all)
+        n_trainList = [800, 1000, 1500, 2000, 2500]
+        n_noiseList = [0, 0.005, 0.01, 0.05, 0.1]
+
+        data_box_num = np.zeros([400, 5])
+        for ii, n_train in enumerate(n_trainList):
+            pred = get_pred(npz_path, n_train, 0, parameter)
+            data_box_num[:, ii] = pred.copy()
+        dict_num.update({parameter: data_box_num})
+        pred = None
+
+        data_box_noise = np.zeros([400, 5])
+        for ii, n_noise in enumerate(n_noiseList):
+            pred = get_pred(npz_path, 2500, n_noise, parameter)
+            data_box_noise[:, ii] = pred.copy()
+        dict_noise.update({parameter: data_box_noise})
+        pred = None
+
+        np.savez(os.path.join("..", "data", "surrogate_data", "GPR_num.npz"), **dict_num)
+        np.savez(os.path.join("..", "data", "surrogate_data", "GPR_noise.npz"), **dict_noise)
+
+    # np.savez(os.path.join("data", "GPR.npz"), **dict_all)
 
 
 
